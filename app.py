fa127323338855ea2b0821124fa3236f10a66e35
Surajram.S <surajram1707@gmail.com>
2021-07-09 09:13:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ import pandas as pd
 from starlette.responses import HTMLResponse 
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+# from model import preProcess_data
+from pydantic import BaseModel
 import tensorflow as tf
 import re
-
-def preProcess_data(text): #cleaning the data
+def preProcess_data(text):
     
     text = text.lower()
     new_text = re.sub('[^a-zA-z0-9\s]','',text)
@@ -22,44 +23,49 @@ tokenizer.fit_on_texts(data['text'].values)
 
 
 
-def my_pipeline(text): #pipeline
+def my_pipeline(text):
   text_new = preProcess_data(text)
   X = tokenizer.texts_to_sequences(pd.Series(text_new).values)
   X = pad_sequences(X, maxlen=28)
   return X
 
 
-@app.get('/') #basic get view
+class inputToModel(BaseModel):
+    text:str
+
+
+@app.get('/')
 def basic_view():
     return {"WELCOME": "GO TO /docs route, or /post or send post request to /predict "}
 
 
 
-@app.get('/predict', response_class=HTMLResponse) #data input by forms
+@app.get('/predict', response_class=HTMLResponse)
 def take_inp():
-    return '''<center><form method="post"> 
+    return '''<form method="post"> 
     <input type="text" maxlength="28" name="text" value="Text Emotion to be tested"/>  
     <input type="submit"/> 
-    </form></center>'''
+    </form>'''
 
 
 
-@app.post('/predict') #prediction on data
-def predict(text:str = Form(...)): #input is from forms
-    clean_text = my_pipeline(text) #cleaning and preprocessing of the texts
-    loaded_model = tf.keras.models.load_model('sentiment.h5') #loading the saved model
-    predictions = loaded_model.predict(clean_text) #making predictions
-    sentiment = int(np.argmax(predictions)) #index of maximum prediction
-    probability = max(predictions.tolist()[0])*100 #probability of maximum prediction
-    if sentiment==0: #assigning appropriate name to prediction
+@app.post('/predict')
+def predict(text:str = Form(...)):
+    clean_text = my_pipeline(text)
+    loaded_model = tf.keras.models.load_model('sentiment.h5')
+    predictions = loaded_model.predict(clean_text)
+    sentiment = int(np.argmax(predictions))
+    probability = max(predictions.tolist()[0])
+    print(sentiment)
+    if sentiment==0:
         t_sentiment = 'negative'
     elif sentiment==1:
         t_sentiment = 'neutral'
     elif sentiment==2:
         t_sentiment='postive'
     
-    return { #returning a dictionary as endpoint
+    return {
         "ACTUALL SENTENCE": text,
         "PREDICTED SENTIMENT": t_sentiment,
-        "Probability Percentage": probability
+        "Probability": probability
     }
